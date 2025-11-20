2814416a92016419138fa8edac384fb706f179c3
Natali Schwartz <schwartznatali@gmail.com>
2025-11-20 10:37:01 -0300

    creados los componentes de Navbar y Navlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
+const queryClient = new QueryClient();
 
-
-
-function App() {
-
-  return (
-    <>
-      <h1 className='bg-red-400'>Hola</h1>
-    </>
-  )
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App
---
+++ src/pages/Index.jsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar";
+
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <main>
+        {/* <Hero />
+        <About />
+        <Services />
+        <Testimonials />
+        <Contact /> */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Index;
